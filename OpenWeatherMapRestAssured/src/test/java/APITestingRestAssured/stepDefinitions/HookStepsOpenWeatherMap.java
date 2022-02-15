891ae186c57d73f0ae8e8662aae3b40f5c25796a
mouditya <99669997+mou1312@users.noreply.github.com>
2022-02-15 08:20:08 +0530

    Added open weather api automation

---
+++ OpenWeatherMapRestAssured/src/test/java/APITestingRestAssured/stepDefinitions/HookStepsOpenWeatherMap.java
@@ -0,0 +1,18 @@
+package APITestingRestAssured.stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class HookStepsOpenWeatherMap {
+
+    @Before
+    public void beforeAny() {
+        System.out.println(" Start Scenario: " + Scenario.class.getName());
+    }
+
+    @After
+    public void afterAny() {
+        System.out.println(" End of Scenario: " + Scenario.class.getName());
+    }
+}
